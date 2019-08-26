bac3f2ca7183d7b8131dab8703d5e7fea496c044
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-08-27 01:26:40 +0200

    Dodano Endpointy do Grades i Subjects

--- Cibrus/Controllers/GradesController.cs
+++ Cibrus/Controllers/GradesController.cs
@@ -17,7 +17,7 @@ namespace Cibrus.Controllers
 
         public GradesController(DatabaseContext context)
         {
-            _context = context;
+           _context = context;
         }
 
         [HttpGet("all/{id}")]
@@ -35,5 +35,40 @@ namespace Cibrus.Controllers
             }
             return Ok(grades);
         }
+
+        [HttpGet("subject/{subject_id}/student/{student_id}")]
+        public IActionResult getgetGradesBySubjaectIdAndStudentId(int subject_id, int student_id)
+        {
+            var grades = _context.grades
+                .Include(g => g.Subject)
+                .Include(g => g.Teacher)
+                .Include(g => g.Student)
+                .Where(b => b.SubjectId.Equals(subject_id))
+                .Where(b => b.StudentId.Equals(student_id));
+
+            if (grades == null)
+            {
+                return BadRequest(new { message = "not found" });
+            }
+            return Ok(grades);
+        }
+
+        [HttpDelete("delete /{grade_id}")]
+        public IActionResult deleteGradeById(int grade_id)
+        {
+            var grade = _context.grades
+                .Where(b => b.GradeId.Equals(grade_id))
+                .FirstOrDefault();
+
+            if (grade == null)
+            {
+                return BadRequest(new { message = "not found" });
+            }
+            _context.grades.Remove(grade);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+
     }
 }
---
+++ Cibrus/Controllers/SubjectController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cibrus.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cibrus.Controllers
+{
+    [Route("api/subjects")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public SubjectController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetSubjects()
+        {
+            var subjects = _context.subjects;
+
+            if (subjects == null)
+            {
+                return BadRequest(new { message = "not found" });
+            }
+            return Ok(subjects);
+        }
+    }
+}
