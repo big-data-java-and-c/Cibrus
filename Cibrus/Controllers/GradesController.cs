512abc592c639e491552d36f9f5e7523c2a0b582
Wiktor Jachimczak <jachimczak3125@gmail.com>
2019-08-26 02:14:56 +0200

    Pierwszy endpoint w Grades Controllerze

---
+++ Cibrus/Controllers/GradesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cibrus.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cibrus.Controllers
+{
+    [Route("api/grade")]
+    [ApiController]
+    public class GradesController : ControllerBase
+    {
+        private readonly DatabaseContext _context;
+
+        public GradesController(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("all/{id}")]
+        public IActionResult GetAllStudentsGrade(int id)
+        {
+            var grades = _context.grades
+                .Include(g => g.Subject)
+                .Include(g => g.Teacher)
+                .Include(g => g.Student)
+                .Where(b => b.StudentId.Equals(id));
+
+            if (grades == null)
+            {
+                return BadRequest(new { message = "not found" });
+            }
+            return Ok(grades);
+        }
+    }
+}
--- Cibrus/Migrations/20190826000041_FullSeededDatabase.Designer.cs
+++ Cibrus/Migrations/20190826000041_FullSeededDatabase.Designer.cs
@@ -3,14 +3,16 @@ using Cibrus.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Cibrus.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190826000041_FullSeededDatabase")]
+    partial class FullSeededDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
