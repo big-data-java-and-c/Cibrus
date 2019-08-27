268d7416cdd984ac7b047afe68a648e4d6d224aa
Kamil Żarski <209526@edu.p.lodz.pl>
2019-08-27 22:26:10 +0200

    added simple user service and auth endpoint

---
+++ Cibrus/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Cibrus.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cibrus.Services
+{
+    public interface IUserService
+    {
+        Response Authenticate(string username, string password);
+        IEnumerable<User> getAllUsers();
+        void addUser(SignUpForm newUser);
+        bool checkIfUserExists(string email);
+        User getUserByUserId(int id);
+        Teacher getTeacherByID(int id);
+    }
+}
---
+++ Cibrus/models/Response.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cibrus.models
+{
+    public class Response
+    {
+        public Token response { get; set; }
+        public long userId { get; set; }
+        public int roleId { get; set; }
+
+        public Response()
+        {
+            response = new Token();
+        }
+    }
+}
